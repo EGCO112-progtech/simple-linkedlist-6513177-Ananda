e204a1b21ff6e2d2f8c1f640c13f99c1114948de
6513177-Ananda <ananda.kon@student.mahidol.edu>
2023-02-08 17:19:02 +0700

    Linked-list which we love.

--- main.c
+++ main.c
@@ -9,55 +9,54 @@
 #include <stdlib.h>
 #include "node.h"
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     int c=5;
-    struct node a,b,*head ;
-    a.value = c;
-    a.next=&b;
-    head=&a;
-    b.value=head->value+3;
+    struct node *head;
 
-    printf("%d\n", head ->value ); //what value for 5
-    printf("%d\n", head ->next->value ); //what value for 8
-/*  Exercise I
-    1. Add 1 more than at the end
-    2. Add value(11)
-    3. Make next become NULL
- */
-    
-/*  Exercise II
-        1. Add 1 more than at the begining!!!!
-        2. Add value (2)
-        
-*/
+    typedef struct node Node;
+    typedef Node* NodePtr;
+
+    NodePtr p = (NodePtr) malloc(sizeof(Node));
+
+    head = p;
+    p->value = c;
+
+    for (int i = 0; i < 4; i++)
+    {
+        p->next = (NodePtr) malloc(sizeof(Node));
+        p = p->next;
+        p->value = c + i + 1;
+        p->next = NULL;
+    }
+
     typedef struct node* NodePtr;
-    NodePtr tmp=head; //add temp value to faciliate
-        
-    /*  Exercise III Use loop to print everything
-        int i,n=5;
-        for(i=0;i<n;i++){
-            printf("%3d", tmp->value);
-          // What is missing???
-        }
-    */
-    
-   /*  Exercise IV change to while loop!! (you can use NULL to help)
-       
-         while(){
-            printf("%3d", tmp->value);
-           // What is missing???
-        }
-    */
-    
- /*  Exercise V Use malloc to create all nodes, instead of create a struct!!
-         //use a loop to help
-          
-     */
+    NodePtr tmp=head; 
 
-    /*  Exercise VI Free all node !!
-         //use a loop to help
-          
-     */
+    for (struct node *ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i\n", ptr->value);
+    }
+    
+    while(tmp != NULL){
+        printf("%3d", tmp->value);
+        tmp = tmp->next;
+    }
+    
+    printf("\n");
+
+    p = head;
+    for (struct node *ptr = p; ptr != NULL; ptr = ptr->next)
+    {
+        printf("%i\n", ptr->value);
+    }
+    
+    tmp = head;
+    while (tmp != NULL)
+    {
+        struct node *next = tmp->next;
+        free(tmp);
+        tmp = next;
+    }
     
     return 0;
 }
